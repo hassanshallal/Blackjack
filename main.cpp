a1a55383a120380f7223022ab0bbf20069bbff2e
“hassanshallal” <hshallal@icloud.com>
2017-08-09 14:05:52 -0700

    Second commit

--- main.cpp
+++ main.cpp
@@ -12,6 +12,3 @@ int main() {
     //Remember to have only one line in main before submission, mentor wants to see a game-controller class that main only runs.
     return 0;
 }
-
-//we managed to solve get around the problem of infinite loop when we enter chars for some switch statements. We need to know how to stop
-// the same problem when we have to input doubles and the user press on letter button. 
